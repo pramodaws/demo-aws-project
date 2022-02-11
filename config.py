e1c7d346f4d89c5cf7d9f42275330a348d7a60d9
PRAMOD B N <pramodbn27@gmail.com>
2022-02-11 10:59:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "mydatabase.ci4td1u42qon.us-east-1.rds.amazonaws.com"
+customhost = "mydatabase123.ci4td1u42qon.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin123"
-customdb = "mydatabase"
+customdb = "mydatabase123"
 custombucket = "my-project-2"
 customregion = "us-east-1"
